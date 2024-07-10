d7be537ae3fc0dad910920d93e1ac9e28275ea30
Muhammad Tahir <mtahirkhanyousufzai@gmail.com>
2024-07-10 15:09:08 +0500

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-        const response = await fetch('http://localhost:3001/api/users/login', {
+        const response = await fetch('https://my-contacts-backend-w86c.onrender.com/api/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
