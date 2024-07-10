ed898dbf800d0b538e2fe48f088e2f1aad06e236
M. Tahir <mtahirkhanyousufzai@gmail.com>
2024-07-10 12:28:13 +0500

    Final Commit

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const {errorHandler} = require('./middleware/errorHandler');
-const connectDB = require('./config/dbConnection')
-const cors = require("cors")
+const { errorHandler } = require('./middleware/errorHandler');
+const connectDB = require('./config/dbConnection');
+const cors = require('cors');
 
 connectDB();
 const app = express();
-const PORT=process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-app.use(cors()); 
+// Corrected CORS configuration
+app.use(cors({
+  origin: "https://hotel-backend-xi.vercel.app",
+  methods: ["POST", "GET", "DELETE", "PUT"],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use("/api/contacts",require("./routers/contacts"))
-app.use("/api/users",require("./routers/users"))
-app.use(errorHandler)
+app.use("/api/contacts", require("./routers/contacts"));
+app.use("/api/users", require("./routers/users"));
+app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    console.log('server is running on port 3001');
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
